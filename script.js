f03c0cffa41d72151bde479619ac0ef2db3e4501
Rudy Brandies <adanb82@gmail.com>
2023-10-04 18:08:59 -0500

    added checkOrder function

--- script.js
+++ script.js
@@ -76,6 +76,20 @@ function swapItems(fromIndex, toIndex) {
     listItems[toIndex].appendChild(itemOne);
 }
 
+function checkOrder() {
+    listItems.forEach((listItem, index) => {
+        const personName = listItem.querySelector('.draggable').innerText.trim();
+
+        if(personName !== richestPeople[index]) {
+            listItem.classList.add('wrong');
+        }
+        else {
+            listItem.classList.remove('wrong');
+            listItem.classList.add('right');
+        }
+    })
+}
+
 function addEventListeners() {
     const draggables = document.querySelectorAll('.draggable');
     const dragListItems = document.querySelectorAll('.draggable-list li');
@@ -89,5 +103,7 @@ function addEventListeners() {
         item.addEventListener('drop', dragDrop);
         item.addEventListener('dragenter', dragEnter);
         item.addEventListener('dragleave', dragLeave);
-    })
+    });
 }
+
+check.addEventListener('click', checkOrder);
